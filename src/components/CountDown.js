14ca834f76718cb8e44e4076c495ef9d2561b95d
Arturo Rodriguez <rodri.art96@gmail.com>
2022-04-05 19:19:00 -0400

    Primer commit de pomodoro app, feature en desarrollo: contador

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-export const CountDown = () => {
+export const CountDown = ({
+    minutes = 20,
+    isPaused,
+}) => {
   return (
     <View>
       <Text> Aqui va el contador </Text>
